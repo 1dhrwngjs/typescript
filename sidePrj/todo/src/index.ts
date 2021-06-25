0dc98a503aeb603174163747eb76a3a918b76315
1dhrwngjs <1dhrwngjs@naver.com>
2021-06-26 02:23:59 +0900

    20210626 // 오류 해결 및 구체적인 타입 정의

--- sidePrj/todo/src/index.ts
+++ sidePrj/todo/src/index.ts
@@ -1,8 +1,8 @@
 //할 일 목록을 받는 배열이기 때문에 : object[] 로 정의
-let todoItems: object[];
+let todoItems: { id: number; title: string; done: boolean }[];
 
 // api, 배열이기 때문에 :object[]
-function fetchTodoItems(): object[] {
+function fetchTodoItems(): { id: number; title: string; done: boolean }[] {
   const todos = [
     { id: 1, title: '안녕', done: false },
     { id: 2, title: '타입', done: false },
@@ -17,7 +17,7 @@ function fetchTodos(): object[] {
   return todos;
 }
 
-function addTodo(todo: object): void {
+function addTodo(todo: { id: number;  title: string; done: boolean }): void {
   todoItems.push(todo);
 }
 
@@ -26,7 +26,10 @@ function deleteTodo(index: number): void {
   todoItems.splice(index, 1);
 }
 
-function completeTodo(index: number, todo: object): void {
+function completeTodo(
+  index: number,
+  todo: { id: number;  title: string; done: boolean }
+): void {
   todo.done = true;
   todoItems.splice(index, 1, todo);
 }
