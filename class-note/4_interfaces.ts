1e010d49c1cb851a1141868712c6fb424fe67a42
1dhrwngjs <1dhrwngjs@naver.com>
2021-06-26 03:05:53 +0900

    20210626 // interface -2

--- class-note/4_interfaces.ts
+++ class-note/4_interfaces.ts
@@ -4,8 +4,56 @@ interface User {
   name: string;
 }
 
-//'seho'란 변수는 'User'란 interface type이 정의 되었다고 볼 수 있음.
+//변수에 interface 활용, 'seho'란 변수는 'User'란 interface type이 정의 되었다고 볼 수 있음.
 var seho: User = {
   age: 33,
   name: '세호'
 }
+
+//함수에 interface 활용
+function getUser(user: User) {
+  console.log(user);
+}
+const capt = {
+  age: 100,
+  name: '캡틴'
+}
+getUser(capt);
+
+// 함수의 구조(스펙)에 interface 활용
+interface SumFunction {
+  (a: number, b: number): number;
+}
+
+var sum: SumFunction;
+sum = function(a: number, b: number): number {
+  return a + b;
+}
+
+// 인덱싱,
+// 위의 코드처럼 정의했던 방식과는 다르다, 'StringArray'라고 하는 속성이 속성 이름이 정해져 있지 않고 'StringArray'를 가져다 사용할때마다 임의로 부여해서 사용할 수 있는 방식
+interface StringArray {
+  [index: number]: string;
+}
+
+var arr: StringArray = ['a','b','c'];
+// arr[0] = 10; // error
+
+// 딕셔너리 패턴, 위 인덱싱 방식과 유사하다.
+interface StringRegexDictionary {
+  [key: string]: RegExp;
+}
+
+var obj: StringRegexDictionary = {
+  // sth: /abc/,
+
+  //.css 확장자를 가지고 있는 모든 파일을 가져오는 정규식
+  cssFile: /\.css$/,
+  jsFile: /\.js$/,
+}
+obj['cssFile'] = 'a' //error
+
+//이 안에 들어오는 값들에 대한 정의가 되었음 (추론이라 표현 함)
+Object.keys(obj).forEach(function(value) {
+
+})
